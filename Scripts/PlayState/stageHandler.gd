a38666a40cfbc536d72910b1ce550e6bbc6972c8
ZennFake <76600019+ZennFake@users.noreply.github.com>
2025-02-03 12:34:38 -0700

    Made boyfriend display properally and added idle animations
    Will add note animations at a later date

--- Scripts/PlayState/stageHandler.gd
+++ Scripts/PlayState/stageHandler.gd
@@ -10,10 +10,12 @@ extends Node
 
 @onready var root = get_parent().get_parent().get_parent()
 @onready var UI = root.get_node("UILock").get_node("UI")
+@onready var Camera : Camera2D = root.get_node("Camera2D")
 
 var conductor
-var metaData
+var metaData : Dictionary
 var stage
+var characterList : Dictionary
 
 ## // FUNCTIONS // ##
 
@@ -28,6 +30,10 @@ func createPlayer(playerString, characterName):
 	spawn.add_child(character)
 	character.position = spawn.position
 	
+	if playerString == "p2":
+		character.get_node("Sprite").flip_h = not character.get_node("Sprite").flip_h
+	
+	self.characterList[playerString] = character
 	
 
 # Starts the stage manager
@@ -47,14 +53,34 @@ func startSong(conductorObject):
 	
 	root.add_child(self.stage)
 	
+	self.characterList = {}
+	
+	if self.metaData.has("p1"):
+		createPlayer("p1", self.metaData["p1"])
+		
 	createPlayer("p2", self.metaData["p2"])
 	
 	
 func beatChanged(beatMajor, beat):
+	# UI Beat
 	self.UI.get_node("BumpAnimator").stop()
 	if beatMajor:
 		self.UI.get_node("BumpAnimator").play("BumpIntense")
 	else:
 		self.UI.get_node("BumpAnimator").play("BumpBasic")
+	
+	# Camera Beat
+	
+	var cameraTween = get_tree().create_tween()
+	cameraTween.tween_property(Camera, "zoom", Vector2(1.05, 1.05), 0)
+	cameraTween.tween_property(Camera, "zoom", Vector2.ONE, 1)
+	cameraTween.play()
+	
+	# Character Bot
+	
+	for playerString in characterList:
+		var character = characterList[playerString]
+		character.get_node("Sprite").stop()
+		character.get_node("Sprite").play("Idle")
 
 ## // OBJECT FUNCTIONS // ##
